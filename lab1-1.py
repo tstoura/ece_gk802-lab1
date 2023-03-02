3082023c10ebc496932c3dae6f793e4e9abcc09d
Theodora Stoura <up1089838@upnet.gr>
2023-03-02 23:35:37 +0200

    lab1: 1,2,3,4

--- lab1-1.py
+++ lab1-1.py
@@ -1,8 +1,22 @@
 import requests
+import time
 
-url = input("Give me a url: ")
+url = input('Give me a url: ')
 
+serv = None
 with requests.get(url) as response:
-    html = response.text
+    hdrs = response.headers.items()
+    print('Headers:')
+    for key,value in hdrs:
+       print("{:30s} {}".format(key,value))
+       if key=="Server":
+           serv = value
 
-print(response.headers)
+    if serv is not None:
+        print()
+        print("Server:" +serv)
+
+    print()
+    print("Cookies")
+    for key,value in enumerate(response.cookies):
+        print("Name:{}   Expires in {} days".format(value.name,round((value.expires-time.time())/86400)))
